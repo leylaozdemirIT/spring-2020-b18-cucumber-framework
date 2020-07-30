b844eac0cdc4359e96120983bc07451ac303b2d5
leylaozdemirIT <mucuku1402@gmail.com>
2020-07-30 08:18:49 -0700

    last

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -1,26 +1,34 @@
 package com.cybertek.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Driver {
 
     //1-Make constructor private
-    private Driver(){
+    private Driver() {
     }
 
     private static WebDriver driver;
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
 
-        if (driver == null){
+        if (driver == null) {
             String browser = ConfigurationReader.getProperty("browser");
 
-            switch (browser){
+            switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
@@ -37,7 +45,32 @@ public class Driver {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
                     break;
+                case "chrome-remote":
+                    try {
+//                        same thing as ChromeOptions
+//                        To request Selenium Grid to run tests on Chrome
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+                        desiredCapabilities.setPlatform(Platform.LINUX);
+//                        ChromeOptions chromeOptions = new ChromeOptions();
+                        URL url = new URL("http://3.235.145.39:4444/wd/hub");
+                        driver = new RemoteWebDriver(url, desiredCapabilities);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "firefox-remote":
+                    try {
+                        //to request grid to run tests on firefox
+                        FirefoxOptions firefoxOptions = new FirefoxOptions();
+                        URL url = new URL("http://3.235.145.39:4444/wd/hub");
+                        driver = new RemoteWebDriver(url, firefoxOptions);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
 
+                default:
+                    throw new RuntimeException("Wrong browser name: "+browser);
             }
         }
 
@@ -45,10 +78,10 @@ public class Driver {
 
     }
 
-    public static void closeDriver(){
-        if (driver != null){
+    public static void closeDriver() {
+        if (driver != null) {
             driver.quit();
-            driver=null;
+            driver = null;
         }
     }
 
